9b30e99bd6a83f6cfb908d6d28bf2ae2aa906759
machaqueso@gmail.com <machaqueso@gmail.com>
2017-05-19 20:49:02 -0500

    Adapted to my streaming config

--- pulse_setup.sh
+++ pulse_setup.sh
@@ -6,23 +6,21 @@
 # pacmd list-sources
 # pacmd list-source-outputs
 # Use pavucontrol to make tests for your setup, and to make the runtime configuration
-# Route you audio source to virtual1
-# Record your sound (videoconference) from virtual2.monitor
 
 MICROPHONE="alsa_input.pci-0000_00_1b.0.analog-stereo"
 SPEAKERS="alsa_output.pci-0000_00_1b.0.analog-stereo"
 
-
 # Create the null sinks
-# virtual1 gets your audio source (mplayer ...) only
-# virtual2 gets virtual1 + micro
-pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
-pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
+# virtual1 is to send line in/mic to discord
+# virtual2 takes all streams you want to mix to your speakers/headphones
+# virtual3 is the audio stream sent to twitch (song requests music)
+pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="speakers_out"
+pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="stream_out"
 
 # Now create the loopback devices, all arguments are optional and can be configured with pavucontrol
+# pactl load-module module-loopback source=$MICROPHONE sink=virtual1
 pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS
-pactl load-module module-loopback source=virtual1.monitor sink=virtual2
-pactl load-module module-loopback source=$MICROPHONE sink=virtual2
+pactl load-module module-loopback source=virtual2.monitor sink=$SPEAKERS
 
 # If you struggle to find the correct values of your physical devices, you can also simply let these undefined, and configure evrything manually via pavucontrol
 # pactl load-module module-loopback source=virtual1.monitor
